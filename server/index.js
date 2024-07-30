293311394818a35396a41fbd2e8cdb3c91fa1aeb
Anideep <128630418+Anideep-Kalia@users.noreply.github.com>
2024-07-30 07:17:54 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+var cors = require('cors')
+
+const app = express()
+const port = 5000
+
+dbConnection()
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
+
+app.use(express.json())
+app.use(cors())
+
+app.use('/api/ticket',require('./routes/superadmin'))      
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
